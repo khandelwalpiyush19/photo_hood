c25d5021745830e0773491826cc0f0a6387ecd4a
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2025-05-25 09:42:31 +0530

    Commit

--- client/src/components/editor/Layers.jsx
+++ client/src/components/editor/Layers.jsx
@@ -18,6 +18,7 @@ const Layers = () => {
         <h3>Layers</h3>
         <p>Select a layer to edit</p>
       </div>
+      
       <div
         onClick={() => handleSelectedLayer("text")}
         className={`layer ${selectedLayer === "text" ? "selected" : ""}`}
